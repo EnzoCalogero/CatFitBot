254299f4d23ff01683fe087b45448ba0e4e2e15e
enzo <enzus@hotmail.com>
2019-03-19 21:57:46 +0000

    cleaning

--- hardware_tester/tilt_switch.py
+++ hardware_tester/tilt_switch.py
@@ -1,9 +1,12 @@
 from gpiozero import Button
-
+import time 
 button = Button(22, pull_up=False)
+#pin= 2 (vv)
+#pin= 15 (pulsante)
 
 while True:
+    time.sleep(1)
     if button.is_pressed:
-        print("The button was pressed!!")
+        print("verticale!")
     else:
-        print("Not pressed")
+         print("exit")
